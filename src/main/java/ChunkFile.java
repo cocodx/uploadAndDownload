4fb537b44ffbba5d793c45f912652a4d3fa480da
刘刚 <2cevening@gmail.com>
2022-08-21 23:32:57 +0800

    feature(root):first commit

--- src/main/java/ChunkFile.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author amazfit
- * @date 2022-08-20 上午3:52
- **/
-public class ChunkFile {
-}
---
+++ src/main/java/io/github/cocodx/UpDownApplication.java
@@ -0,0 +1,16 @@
+package io.github.cocodx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author amazfit
+ * @date 2022-08-21 下午10:52
+ **/
+@SpringBootApplication
+public class UpDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpDownApplication.class,args);
+    }
+}
