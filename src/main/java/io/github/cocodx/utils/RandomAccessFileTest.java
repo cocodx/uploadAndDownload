bc1ce5d261b8eccfcb2a546fcc7cb3da5c86c664
刘刚 <2cevening@gmail.com>
2022-08-27 03:07:28 +0800

    feature(*):添加单线程读写，多个线程读写

--- src/main/java/io/github/cocodx/utils/RandomAccessFileTest.java
+++ src/main/java/io/github/cocodx/utils/RandomAccessFileTest.java
@@ -1,7 +1,7 @@
 package io.github.cocodx.utils;
 
-import java.io.File;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  * @author amazfit
@@ -9,26 +9,81 @@ import java.io.RandomAccessFile;
  **/
 public class RandomAccessFileTest {
 
-    public static void main(String[] args)throws Exception {
-        File file = new File("D:\\hello.txt");
-        RandomAccessFile raf = new RandomAccessFile(file, "r");
+    public static void main1(String[] args)throws Exception {
+        File file = new File("E:\\fuck\\vec-380.mp4");
 
-        //已经读取了三个字节
-        int a = raf.read();
-        System.out.println((char)a);
-        raf.read();
-        raf.read();
-
-
-        //从文件的第6个字节开始
-        raf.skipBytes(5);//相对位置 当前字符串。原文本读一下就少一下
-//        raf.seek(5);//绝对位置
+        FileInputStream fileInputStream = new FileInputStream(file);
+        FileOutputStream fileOutputStream = new FileOutputStream("G:\\11.mp4");
 
         byte[] bytes = new byte[1024 * 8];
+        int len;
+        long start = System.currentTimeMillis();
+        while ((len = fileInputStream.read(bytes))!=-1){
+            fileOutputStream.write(bytes,0,len);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
 
-        //读取文件的内容，一次读8kb
-        int length = raf.read(bytes);
+        long end = System.currentTimeMillis();
+        System.out.println((end-start)/1000);
+    }
 
-        System.out.println(new String(bytes,0,length));
+    public static void main(String[] args)throws Exception {
+        File file = new File("E:\\fuck\\vec-380.mp4");
+        File file1 = new File("G:\\11.mp4");
+        //开启5个线程
+        int threadNum = 5;
+
+        long length = file.length();
+        //向上取整
+        int part = (int) Math.ceil(length / threadNum);
+        //阻塞主线程，当子线程都走完了，再走主线程
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < threadNum; i++) {
+            final int k = i;
+            Thread thread = new Thread(() -> {
+                //线程具体做的事情
+                try {
+                    RandomAccessFile readFile = new RandomAccessFile(file, "r");
+                    RandomAccessFile writeFile = new RandomAccessFile(file1, "rw");
+                    //从指定位置读
+                    readFile.seek(k * part);
+                    writeFile.seek(k * part);
+
+                    byte[] bytes = new byte[1024 * 8];
+                    int len = -1, plen = 0;
+                    while (true) {
+                        len = readFile.read(bytes);
+                        //读完了，退出循环
+                        if (len == -1) {
+                            break;
+                        }
+                        //如果不等于-1,则累加求和
+                        plen = plen + len;
+
+                        //将读取的数据进行写入
+                        writeFile.write(bytes, 0, len);
+
+                        if (plen >= part) {
+                            break;
+                        }
+                    }
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            thread.start();
+            //把这5个线程保存到集合中
+            threads.add(thread);
+        }
+        long begin = System.currentTimeMillis();
+        for (Thread thread:threads){
+            thread.join();//将线程加入，并阻塞主线程
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("拷贝消耗了，"+(end-begin)/1000);
     }
 }
